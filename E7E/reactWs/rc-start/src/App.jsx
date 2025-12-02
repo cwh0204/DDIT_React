6acb0b7e05f039217586f9e3326eac9c3265a381
cwh0204 <cwh0204@nate.com>
2025-12-02 21:46:31 +0900

    2025-12-02
    2025-12-02

--- E7E/reactWs/rc-start/src/App.jsx
+++ E7E/reactWs/rc-start/src/App.jsx
@@ -1,32 +1,31 @@
-import React,{useState,useEffect} from "react";
+import React,{useRef,useEffect,useState} from "react";
 import IdolList from "./IdolList";
 
 function App() {
-  //props를 이용해서 자식에게 부모의 함수를 넘겨 줌
 
-  const[postData, setPostData] = useState("로딩 중...");
-  const[userId, setUserId] = useState(1);
+  const ref = useRef(null);
 
-  let timer = () => {
-      setTimeout(() => {
-        setPostData(`사용자 ${userId}의 최신 포스트 정보입니다. (시간: ${new Date().toLocaleTimeString()})`);
-      }, 1000);
-  }
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    setPostData("불러오는중..");
-    timer();
-    return clearTimeout(timer);
-  },[userId]);
+    ref.current.focus();
+    console.log(ref.current);
+  },[]);
 
-  const test = () => {
-    setUserId(userId + 1);
-  }
+  useEffect(() => {
+    const test = document.querySelector("#test");
+    test.innerHTML = count;
+  },[count]);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
 
   return (
     <> 
-      <button onClick={test}>버튼 클릭 + 1</button>
-      {postData}
+      <button onClick={handleClick}>버튼 클릭 + 1</button>
+      <input type="text" ref={ref}/>
+      <div id = "test"></div>
       {/* <React.Fragment> */}
       {/* 실제 이렇게 처리하는 것은 좋은 방법이 아님, 이것은 하드코딩!
       <Idol name={"지원"} song={"Null 지원할게"}/>
